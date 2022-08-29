9f99ef21d1eed79291ec4d6ed7057d173d62fa75
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2022-08-29 22:05:57 +0300

    Add files via upload

---
+++ ForLoop/P09LeftAndRightSum.java
@@ -0,0 +1,45 @@
+package L04ForLoop;
+
+import java.util.Scanner;
+
+public class P09LeftAndRightSum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int leftSum = 0;
+        for (int i = 1; i <= n; i++) {
+            int currentNum = Integer.parseInt(scanner.nextLine());
+
+            leftSum = leftSum + currentNum;
+        }
+
+        int rightSum = 0;
+        for (int i = 1; i <= n; i++) {
+            int currentNum = Integer.parseInt(scanner.nextLine());
+
+            rightSum = rightSum + currentNum;
+        }
+
+//        int leftSum = 0;
+//        int rightSum = 0;
+//        for (int i = 1; i <= 2 * n; i++) {
+//            int currentNum = Integer.parseInt(scanner.nextLine());
+//            if (i <= n) {
+//                leftSum += currentNum;
+//            } else {
+//                rightSum += currentNum;
+//            }
+//        }
+
+        if (leftSum == rightSum) {
+            System.out.printf("Yes, sum = %d", leftSum);
+        } else {
+            int diff = Math.abs(leftSum - rightSum);
+            System.out.printf("No, diff = %d", diff);
+        }
+
+    }
+}

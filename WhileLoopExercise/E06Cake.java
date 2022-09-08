73d6795e58537eeb17acc19a679e3b458857a4ee
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2022-09-08 18:40:49 +0300

    Add files via upload

---
+++ WhileLoopExercise/E06Cake.java
@@ -0,0 +1,35 @@
+package P05WhileLoopExercise;
+
+import java.util.Scanner;
+
+public class E06Cake {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int width = Integer.parseInt(scanner.nextLine());
+        int length = Integer.parseInt(scanner.nextLine());
+
+        int cakePieces = width * length;
+
+        String command = scanner.nextLine();
+        boolean noMoreCake = false;
+        while (!command.equals("STOP")) {
+            int pieces = Integer.parseInt(command);
+            cakePieces = cakePieces - pieces;
+
+            if (cakePieces <= 0) {
+                noMoreCake = true;
+                break;
+            }
+
+            command = scanner.nextLine();
+        }
+
+        if (noMoreCake) {
+            System.out.printf("No more cake left! You need %d pieces more.", Math.abs(cakePieces));
+        } else {
+            System.out.printf("%d pieces are left.", cakePieces);
+        }
+
+    }
+}

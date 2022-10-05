930527739889222e3b9191a2edc4911c6c61663c
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2022-10-05 13:04:18 +0300

    Create E08TriangleOfNumbers.java

---
+++ Programming-Fundamentals-Java-September-2022/basicSyntaxExercise/E08TriangleOfNumbers.java
@@ -0,0 +1,20 @@
+package basicSyntaxExercise;
+
+import java.util.Scanner;
+
+public class E08TriangleOfNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int row = 1; row <= n ; row++) {
+            //колко пъти да печатам нещо
+            for (int count = 1; count <= row ; count++) {
+                System.out.print(row + " ");
+            }
+            System.out.println();
+
+        }
+    }
+}

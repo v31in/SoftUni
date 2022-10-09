ff87e2085808a5b1e5769b5819c18d38328130ed
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2022-10-09 19:35:04 +0300

    Create P11RefactorVolumeOfPyramid.java

---
+++ Programming-Fundamentals-Java-September-2022/dataTypesAndVariablesLab/P11RefactorVolumeOfPyramid.java
@@ -0,0 +1,19 @@
+package dataTypesAndVariablesLab;
+
+import java.util.Scanner;
+
+public class P11RefactorVolumeOfPyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double length, width, height = 0;
+        System.out.print("Length: ");
+        length = Double.parseDouble(scanner.nextLine());
+        System.out.print("Width: ");
+        width = Double.parseDouble(scanner.nextLine());
+        System.out.print("Height: ");
+        height = Double.parseDouble(scanner.nextLine());
+        double Volume = (length * width * height) / 3;
+        System.out.printf("Pyramid Volume: %.2f", Volume);
+    }
+}

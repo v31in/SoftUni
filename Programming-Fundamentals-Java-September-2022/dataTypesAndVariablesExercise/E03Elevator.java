008378ce97e0d6c462bd95128e69ed453c2210e9
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2022-10-14 23:12:20 +0300

    Create E03Elevator.java

---
+++ Programming-Fundamentals-Java-September-2022/dataTypesAndVariablesExercise/E03Elevator.java
@@ -0,0 +1,16 @@
+package dataTypesAndVariablesExercise;
+
+import java.util.Scanner;
+
+public class E03Elevator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int countPeople = Integer.parseInt(scanner.nextLine());
+        int capacity = Integer.parseInt(scanner.nextLine());
+
+        //курсове = хората / капацитета -> дробно число -> нагоре -> отпечатвам
+        double courses = Math.ceil(countPeople * 1.0 / capacity);
+        System.out.printf("%.0f", courses);
+    }
+}

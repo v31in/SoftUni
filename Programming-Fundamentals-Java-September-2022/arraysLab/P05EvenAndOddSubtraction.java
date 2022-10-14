0637550c2398114f83e442b93029c023481afc8b
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2022-10-15 00:20:54 +0300

    Create P05EvenAndOddSubtraction.java

---
+++ Programming-Fundamentals-Java-September-2022/arraysLab/P05EvenAndOddSubtraction.java
@@ -0,0 +1,27 @@
+package arraysLab;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class P05EvenAndOddSubtraction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbersArr = Arrays
+                .stream(scanner.nextLine().split(" "))
+                .mapToInt(e -> Integer.parseInt(e))
+                .toArray();
+
+        int evenSum = 0;
+        int oddSum = 0;
+        for (int item : numbersArr) {
+            if (item % 2 == 0) {
+                evenSum += item;
+            } else {
+                oddSum += item;
+            }
+        }
+
+        System.out.println(evenSum - oddSum);
+    }
+}

73015dff1974fb424b9f728a3326e209a7c209c4
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2022-11-26 13:22:40 +0200

    Create E10SoftUniExamResults.java

---
+++ Programming-Fundamentals-Java-September-2022/associativeArraysExercise/E10SoftUniExamResults.java
@@ -0,0 +1,56 @@
+package associativeArraysExercise;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class E10SoftUniExamResults {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //username -> points
+        Map<String, Integer> userPoints = new LinkedHashMap<>();
+        // language -> count
+        Map<String, Integer> languageCount = new LinkedHashMap<>();
+        String input = scanner.nextLine();
+
+        while (!input.equals("exam finished")) {
+            String[] splitArray = input.split("-");
+            //1. "{username}-{language}-{points}" -> ["{username}", "{language}", "{points}"]
+            //2. "{username}-banned" -> ["{username}", "banned"]
+            String username = splitArray[0];
+            if (splitArray.length == 3) {
+                String language = splitArray[1];
+                int points = Integer.parseInt(splitArray[2]);
+                //добавяме username и точки
+                if (!userPoints.containsKey(username)) {
+                    userPoints.put(username, points);
+                } else {
+                    int currentPoints = userPoints.get(username);
+                    if (points > currentPoints) {
+                        userPoints.put(username, points);
+                    }
+                }
+
+                //добавяме брой срещания на езика
+                if (!languageCount.containsKey(language)) {
+                    languageCount.put(language, 1);
+                } else {
+                    languageCount.put(language, languageCount.get(language) + 1);
+                }
+
+            } else {
+                userPoints.remove(username);
+            }
+            input = scanner.nextLine();
+        }
+
+        System.out.println("Results:");
+        userPoints.entrySet().stream()
+                .forEach(e -> System.out.println(e.getKey() + " | " + e.getValue()));
+
+        System.out.println("Submissions:");
+        languageCount.entrySet().stream()
+                .forEach(e -> System.out.println(e.getKey() + " - " + e.getValue()));
+    }
+}

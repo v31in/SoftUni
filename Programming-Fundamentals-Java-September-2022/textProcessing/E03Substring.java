87284acadd3d985f3e66bd0d1c8e456ab15afa6f
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2022-11-30 22:32:20 +0200

    Create E03Substring.java

---
+++ Programming-Fundamentals-Java-September-2022/textProcessing/E03Substring.java
@@ -0,0 +1,20 @@
+package textProcessing;
+
+import java.util.Scanner;
+
+public class E03Substring {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String toRemove = scanner.nextLine();
+        String text = scanner.nextLine();
+
+        int index = text.indexOf(toRemove);
+        while (index != -1) {
+            text = text.replace(toRemove, "");
+            index = text.indexOf(toRemove);
+        }
+
+        System.out.println(text);
+    }
+}

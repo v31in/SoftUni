5475ecc05a104508986474542681048f09066228
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2022-11-30 22:31:52 +0200

    Create E01ReverseStrings.java

---
+++ Programming-Fundamentals-Java-September-2022/textProcessing/E01ReverseStrings.java
@@ -0,0 +1,23 @@
+package textProcessing;
+
+import java.util.Scanner;
+
+public class E01ReverseStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        while (!input.equals("end")) {
+
+            String reversedWord = "";
+            for (int i = input.length() - 1; i >= 0; i--) {
+                char currentSymbol = input.charAt(i);
+                reversedWord = reversedWord + currentSymbol;
+            }
+
+            System.out.printf("%s = %s%n", input, reversedWord);
+
+            input = scanner.nextLine();
+        }
+    }
+}

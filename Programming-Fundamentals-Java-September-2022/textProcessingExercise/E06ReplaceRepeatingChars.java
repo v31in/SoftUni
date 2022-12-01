504892db2f8d0c0dd863c0120e491dfd9147a951
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2022-12-01 23:05:36 +0200

    Create E06ReplaceRepeatingChars.java

---
+++ Programming-Fundamentals-Java-September-2022/textProcessingExercise/E06ReplaceRepeatingChars.java
@@ -0,0 +1,27 @@
+package textProcessingExercise;
+
+import java.util.Scanner;
+
+public class E06ReplaceRepeatingChars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        String res = "";
+
+        char firstOccur = input.charAt(0);
+        res += firstOccur;
+
+        for (int i = 1; i < input.length(); i++) {
+            char currentSymbol = input.charAt(i);
+            if (currentSymbol != firstOccur) {
+                res += currentSymbol;
+                firstOccur = currentSymbol;
+            }
+
+        }
+
+        System.out.println(res);
+    }
+}

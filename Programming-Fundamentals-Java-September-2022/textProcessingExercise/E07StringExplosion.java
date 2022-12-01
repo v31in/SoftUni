1624c9a87479b352599fda12e3db721424c91767
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2022-12-01 23:05:57 +0200

    Create E07StringExplosion.java

---
+++ Programming-Fundamentals-Java-September-2022/textProcessingExercise/E07StringExplosion.java
@@ -0,0 +1,29 @@
+package textProcessingExercise;
+
+import java.util.Scanner;
+
+public class E07StringExplosion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine(); //"abv>1>1>2>2asdasd"
+        StringBuilder textBuilder = new StringBuilder(input);
+        int totalStrength = 0; //сила
+        for (int position = 0; position <= textBuilder.length() - 1; position++) {
+            char currentSymbol = textBuilder.charAt(position);
+            if (currentSymbol == '>') {
+                //експлозия
+                //char ('1') -> "1" -> int 1
+                int explosionStrength = Integer.parseInt(textBuilder.charAt(position + 1) + ""); //сила на експлозия
+                totalStrength += explosionStrength;
+            } else if (currentSymbol != '>' && totalStrength > 0) {
+                //премахвам
+                textBuilder.deleteCharAt(position);
+                totalStrength--;
+                position--;
+            }
+        }
+
+        System.out.println(textBuilder);
+    }
+}

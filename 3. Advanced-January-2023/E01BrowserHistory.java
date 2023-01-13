6c2556e161a7d64322c3da43fcfbae795f381191
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-01-13 17:17:00 +0200

    Create E01BrowserHistory.java

---
+++ 3. Advanced-January-2023/E01BrowserHistory.java
@@ -0,0 +1,36 @@
+package stacksAndQueuesLab;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class E01BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Deque<String> history =  new ArrayDeque<>();
+
+        String currentInput = scanner.nextLine();
+
+        while (!currentInput.equals("Home")) {
+            if (currentInput.equals("back")) {
+                if (history.isEmpty() || history.size() == 1) {
+                    System.out.println("no previous URLs");
+
+                    currentInput = scanner.nextLine();
+
+                    continue;
+                }
+
+                history.pop();
+            } else {
+                history.push(currentInput);
+            }
+
+            System.out.println(history.peek());
+
+            currentInput = scanner.nextLine();
+        }
+
+    }
+}

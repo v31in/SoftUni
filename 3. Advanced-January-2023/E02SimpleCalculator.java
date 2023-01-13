73b6b5d19f436fbae60470d5b94e0621610cde5a
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-01-13 17:17:21 +0200

    Create E02SimpleCalculator.java

---
+++ 3. Advanced-January-2023/E02SimpleCalculator.java
@@ -0,0 +1,42 @@
+package stacksAndQueuesLab;
+
+import java.util.*;
+
+public class E02SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<String> stack = new ArrayDeque<>();
+
+        String expression = scanner.nextLine();
+        String[] expressionParts = expression.split(" ");
+        List<String> partsList = Arrays.asList(expressionParts);
+
+//        stack.addAll(partsList);
+
+        Collections.reverse(partsList);
+
+        for (String part : partsList) {
+            stack.push(part);
+        }
+
+        while (stack.size() > 1) {
+            int first = Integer.parseInt(stack.pop());
+            String op = stack.pop();
+            int second = Integer.parseInt(stack.pop());
+
+            int result;
+            switch (op) {
+                case "+": result = first + second; break;
+                case "-": result = first - second; break;
+                default:
+                    System.out.println("Unknown operation " + op);
+                    return;
+            }
+
+            stack.push("" + result);
+        }
+
+        System.out.println(stack.peek());
+    }
+}

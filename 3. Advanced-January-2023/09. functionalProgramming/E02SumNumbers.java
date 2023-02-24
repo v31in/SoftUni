10d01812a8f5da00d34b95edb77dc596b3135eed
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-02-24 22:49:45 +0200

    Create E02SumNumbers.java

---
+++ 3. Advanced-January-2023/09. functionalProgramming/E02SumNumbers.java
@@ -0,0 +1,27 @@
+package functionalProgramming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class E02SumNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Function<String, Integer> parse = s -> Integer.parseInt(s);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(", "))
+                .map(elem -> parse.apply(elem))
+                .collect(Collectors.toList());
+
+        int sum = numbers
+                .stream()
+                .mapToInt(a -> a)
+                .sum();
+
+        System.out.println("Count = " + numbers.size());
+        System.out.println("Sum = " + sum);
+    }
+}

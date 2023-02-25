b2fb912dbf51af898d7f1b3ab8afd2d1683c3aca
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-02-25 16:24:23 +0200

    Create E01ConsumerPrint.java

---
+++ 3. Advanced-January-2023/10. functionalProgrammingExercises/E01ConsumerPrint.java
@@ -0,0 +1,40 @@
+package functionalProgrammingExercises;
+
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class E01ConsumerPrint {
+    public static void main(String[] args) {
+        //Function<приема, връща> -> apply
+        //Consumer<приема> -> void -> accept
+        //Supplier<връща> -> get
+        //Predicate<приема> -> връща true / false -> test
+        //BiFunction<приема1, приема2, връща> -> apply
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine(); //"Peter George Alex"
+        String[] names = input.split("\\s+"); //["Peter", "George", "Alex"]
+
+        //начин 1: без functional programming
+        /*for (String name : names) {
+            System.out.println(name);
+        }*/
+
+        //начин 2:
+        //име -> печатам
+        /*Consumer<String> printName = name -> System.out.println(name);
+        //printName приема String name и я отпечатва
+        for (String name : names) {
+            printName.accept(name);
+        }*/
+
+        //начин 3:
+        //масив с имена -> печатам всяко едно име
+        Consumer<String[]> printArray = array -> {
+            for (String name : array) {
+                System.out.println(name);
+            }
+        };
+        //printArray приема масив от текстове и го отпечатва
+        printArray.accept(names);
+    }
+}

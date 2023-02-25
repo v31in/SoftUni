08e738b6126d6621f8120e6d4e3726b9cd02d01a
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-02-25 16:25:04 +0200

    Create E02KnightsOfHonor.java

---
+++ 3. Advanced-January-2023/10. functionalProgrammingExercises/E02KnightsOfHonor.java
@@ -0,0 +1,26 @@
+package functionalProgrammingExercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class E02KnightsOfHonor {
+    public static void main(String[] args) {
+        //Function<приема, връща> -> apply
+        //Consumer<приема> -> void -> accept
+        //Supplier<връща> -> get
+        //Predicate<приема> -> връща true / false -> test
+        //BiFunction<приема1, приема2, връща> -> apply
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine(); //"Peter George Alex"
+        String[] names = input.split("\\s+"); //["Peter", "George", "Alex"]
+
+        //име -> отпечатам "Sir " + име
+        Consumer<String> printName = name -> System.out.println("Sir " + name);
+        Arrays.stream(names).forEach(printName);
+
+        /*for (String name : names) {
+            printName.accept(name);
+        }*/
+    }
+}

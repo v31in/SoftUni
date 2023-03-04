7ff165483dc1f4f680e22c89742238c953300d04
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-03-05 00:08:57 +0200

    Create Child.java

---
+++ 3. Advanced-January-2023/12. definingClassesExercises/google/Child.java
@@ -0,0 +1,16 @@
+package definingClassesExercises.google;
+
+public class Child {
+    private String childName;
+    private String childBirthday;
+
+    public Child(String childName, String childBirthday) {
+        this.childName = childName;
+        this.childBirthday = childBirthday;
+    }
+
+    @Override
+    public String toString() {
+        return childName + " " + childBirthday;
+    }
+}

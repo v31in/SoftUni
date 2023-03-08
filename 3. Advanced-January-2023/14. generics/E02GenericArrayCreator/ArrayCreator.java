acb684630d389f0e588b32523300497cc76f5ea0
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-03-08 15:43:30 +0200

    Create ArrayCreator.java

---
+++ 3. Advanced-January-2023/14. generics/E02GenericArrayCreator/ArrayCreator.java
@@ -0,0 +1,17 @@
+package generics.E02GenericArrayCreator;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ArrayCreator {
+    public static <T> T[] create(int length, T item) {
+        T[] array = (T[]) new Object[length];
+        Arrays.fill(array, item);
+        return array;
+    }
+    public static <T> T[] create(Class<T> cl, int length, T item) {
+        T[] array = (T[]) Array.newInstance(cl, length);
+        Arrays.fill(array, item);
+        return array;
+    }
+}

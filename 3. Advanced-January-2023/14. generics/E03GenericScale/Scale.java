a009d9c1ef4b2b82d60d80a7236ed2a9f161ef62
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-03-08 15:44:22 +0200

    Create Scale.java

---
+++ 3. Advanced-January-2023/14. generics/E03GenericScale/Scale.java
@@ -0,0 +1,21 @@
+package generics.E03GenericScale;
+
+public class Scale <T extends Comparable<T>>{
+    private final T left;
+    private final T right;
+
+    public Scale(T left, T right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public T getHeavier() {
+        if (this.left.compareTo(this.right) == 0) {
+            return null;
+        } else if (this.left.compareTo(this.right) < 0) {
+            return this.right;
+        } else {
+            return this.left;
+        }
+    }
+}

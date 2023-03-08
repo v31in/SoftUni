c63ed3d3677951eb1a1d51fbf52b3c7bfc403bdd
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-03-08 15:42:55 +0200

    Create Jar.java

---
+++ 3. Advanced-January-2023/14. generics/E01JarOfT/Jar.java
@@ -0,0 +1,19 @@
+package generics.E01JarOfT;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Jar<T> {
+    private final Deque<T> content;
+    public Jar() {
+        this.content = new ArrayDeque<>();
+    }
+
+    public void add(T element) {
+        this.content.push(element);
+    }
+
+    public T remove() {
+        return this.content.pop();
+    }
+}

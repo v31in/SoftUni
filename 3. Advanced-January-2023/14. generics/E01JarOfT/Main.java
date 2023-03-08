6718269b5508f19c972d1e8163f83bef1a3a996d
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-03-08 15:43:03 +0200

    Create Main.java

---
+++ 3. Advanced-January-2023/14. generics/E01JarOfT/Main.java
@@ -0,0 +1,10 @@
+package generics.E01JarOfT;
+
+public class Main {
+    public static void main(String[] args) {
+        Jar<String> deque = new Jar<>();
+
+        deque.add("asd");
+        System.out.println(deque.remove());
+    }
+}

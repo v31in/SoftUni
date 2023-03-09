164055906b7e642980b96fe44b61238240972636
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-03-09 23:35:39 +0200

    E02GenericBoxOfInteger

---
+++ 3. Advanced-January-2023/15. genericsExercise/E02GenericBoxOfInteger/Box.java
@@ -0,0 +1,14 @@
+package genericsExercise.E02GenericBoxOfInteger;
+
+public class Box<T> {
+    private final T element;
+
+    public Box(T element) {
+        this.element = element;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(this.element.getClass().getName() + ": " + this.element);
+    }
+}

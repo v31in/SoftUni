c0c5927cf2d5f820f09a55e0b16c188601fc9041
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-03-11 12:09:35 +0200

    E01Book

---
+++ 3. Advanced-January-2023/16. iteratorsAndComparators/E01Book/Main.java
@@ -0,0 +1,11 @@
+package iteratorsAndComparators.E01Book;
+
+public class Main {
+    public static void main(String[] args) {
+        Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+        Book bookThree = new Book("The Documents in the Case", 2002);
+        Book bookTwo = new Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
+
+        System.out.println();
+    }
+}

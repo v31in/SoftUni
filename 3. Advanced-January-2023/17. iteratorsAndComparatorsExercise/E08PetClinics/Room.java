baffb78813513678124551cc438f2a532bbe4b06
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-03-12 12:10:27 +0200

    E08PetClinics

---
+++ 3. Advanced-January-2023/17. iteratorsAndComparatorsExercise/E08PetClinics/Room.java
@@ -0,0 +1,18 @@
+package iteratorsAndComparatorsExercise.E08PetClinics;
+
+public class Room {
+    public Pet pet;
+
+    public Room() {
+        this.pet = null;
+    }
+
+    @Override
+    public String toString() {
+        if (pet == null) {
+            return "Room empty";
+        } else {
+            return pet.toString();
+        }
+    }
+}

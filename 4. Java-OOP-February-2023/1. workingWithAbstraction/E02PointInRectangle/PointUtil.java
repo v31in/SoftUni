d83445052eaa94187a13776ebfb7c8d714a8b8d1
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-03-14 00:58:56 +0200

    E02PointInRectangle

---
+++ 4. Java-OOP-February-2023/1. workingWithAbstraction/E02PointInRectangle/PointUtil.java
@@ -0,0 +1,19 @@
+package workingWithAbstraction.E02PointInRectangle;
+
+public class PointUtil {
+    /**
+     * Split " ", int coordinates
+     *
+     *
+     * @param input
+     * @return
+     */
+    public static Point parsePoint(String input) {
+        String[] parts = input.split(" ");
+
+        int x = Integer.parseInt(parts[0]);
+        int y = Integer.parseInt(parts[1]);
+
+        return new Point(x, y);
+    }
+}

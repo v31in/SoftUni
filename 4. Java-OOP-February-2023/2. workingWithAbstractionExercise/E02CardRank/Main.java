31faf2b125fd479dc088f2bfa7a1439e625faddf
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-03-15 00:12:40 +0200

    E02CardRank

---
+++ 4. Java-OOP-February-2023/2. workingWithAbstractionExercise/E02CardRank/Main.java
@@ -0,0 +1,34 @@
+package workingWithAbstractionExercise.E02CardRank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            String firstLine = reader.readLine();
+
+            if (firstLine.equals("Card Suits")) {
+                printSuits();
+            } else if (firstLine.equals("Card Ranks")) {
+                printRanks();
+            }
+        }
+
+        private static void printRanks() {
+            System.out.println("Card Ranks:");
+
+            for (Cards.Ranks rank : Cards.Ranks.values()) {
+                System.out.printf("Ordinal value: %d; Name value: %s%n", rank.getValue(), rank);
+            }
+        }
+        private static void printSuits() {
+            System.out.println("Card Suits:");
+
+            for (Cards.Suits suit : Cards.Suits.values()) {
+                System.out.printf("Ordinal value: %d; Name value: %s%n", suit.getValue(), suit);
+            }
+    }
+}

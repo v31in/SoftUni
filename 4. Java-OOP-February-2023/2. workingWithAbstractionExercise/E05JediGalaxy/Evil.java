048db2101d5da7f40ac57a99cf65c50e34400bf7
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-03-15 00:13:19 +0200

    E05JediGalaxy

---
+++ 4. Java-OOP-February-2023/2. workingWithAbstractionExercise/E05JediGalaxy/Evil.java
@@ -0,0 +1,23 @@
+package workingWithAbstractionExercise.E05JediGalaxy;
+
+public class Evil {
+    private int row;
+    private int column;
+
+
+    public Evil(int[] coordinates) {
+        this.row = coordinates[0];
+        this.column = coordinates[1];
+    }
+
+    public void move(Field field) {
+        while (row >= 0 && column >= 0) {
+            if (row < field.getLength()
+                    && column < field.getInnerLength(row)) {
+                field.setFieldValue(row, column, 0);
+            }
+            row--;
+            column--;
+        }
+    }
+}
---
+++ 4. Java-OOP-February-2023/2. workingWithAbstractionExercise/E05JediGalaxy/Galaxy.java
@@ -0,0 +1,21 @@
+package workingWithAbstractionExercise.E05JediGalaxy;
+
+public class Galaxy {
+    private Field field;
+    private Evil evil;
+    private Player player;
+
+    public Galaxy(Field field, Evil evil, Player player) {
+        this.field = field;
+        this.evil = evil;
+        this.player = player;
+    }
+
+    public void moveEvil() {
+        evil.move(this.field);
+    }
+
+    public void movePlayer() {
+        player.move(field);
+    }
+}

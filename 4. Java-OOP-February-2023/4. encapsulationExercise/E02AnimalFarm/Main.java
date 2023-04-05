8ef26a960d17ecc6969cbc04e104f3196719cede
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-04-05 18:19:06 +0300

    E02AnimalFarm

---
+++ 4. Java-OOP-February-2023/4. encapsulationExercise/E02AnimalFarm/Main.java
@@ -0,0 +1,19 @@
+package encapsulationExercise.E02AnimalFarm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String name = reader.readLine();
+        int age = Integer.parseInt(reader.readLine());
+
+        Chicken chicken = new Chicken(name, age);
+
+        System.out.println(chicken);
+
+    }
+}

e531c0a323048bbcfec47c468b315ec900346b5e
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-04-07 19:31:42 +0300

    E01Person

---
+++ 4. Java-OOP-February-2023/6. inheritanceExercise/E01Person/Person.java
@@ -0,0 +1,27 @@
+package inheritanceExercise.E01Person;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    private void setAge(int age) {
+        this.age = age;
+    }
+}

d57762565bc1568a724f62552c49d8bd45ee7f9c
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-04-07 19:32:39 +0300

    E06Animals

---
+++ 4. Java-OOP-February-2023/6. inheritanceExercise/E06Animals/Kitten.java
@@ -0,0 +1,14 @@
+package inheritanceExercise.E06Animals;
+
+public class Kitten extends Cat {
+    public static final String DEFAULT_GENDER = "Female";
+
+    public Kitten(String name, int age) {
+        super(name, age, DEFAULT_GENDER);
+    }
+
+    @Override
+    public String produceSound() {
+        return "Meow";
+    }
+}

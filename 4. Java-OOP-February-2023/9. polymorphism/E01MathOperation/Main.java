2ac8b3e51029fb3dbfc2f22be01960b4a2e04d62
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-05-02 00:59:29 +0300

    E01MathOperation

---
+++ 4. Java-OOP-February-2023/9. polymorphism/E01MathOperation/Main.java
@@ -0,0 +1,14 @@
+package polymorphism.E01MathOperation;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MathOperation math = new MathOperation();
+        System.out.println(math.add(2, 2));
+        System.out.println(math.add(3, 3, 3));
+        System.out.println(math.add(4, 4, 4, 4));
+
+        MathOperationExtended mathOperationExtended = new MathOperationExtended();
+
+    }
+}
---
+++ 4. Java-OOP-February-2023/9. polymorphism/E01MathOperation/MathOperationExtended.java
@@ -0,0 +1,8 @@
+package polymorphism.E01MathOperation;
+
+public class MathOperationExtended extends MathOperation {
+
+    public long add(int a, long b) {
+        return a + b;
+    }
+}

2a3aade77be18a50ca622d9c762a6683c6e40e9d
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2023-05-09 21:17:54 +0300

    exceptionsAndErrorHandling

---
+++ 4. Java-OOP-February-2023/13. exceptionsAndErrorHandling/E03EnterNumbers.java
@@ -0,0 +1,51 @@
+package exceptionsAndErrorHandling;
+
+import exceptionsAndErrorHandling.exeptions.NumbersException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class E03EnterNumbers {
+    private static Scanner scanner;
+
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+
+        List<Integer> numbers = new ArrayList<>();
+
+        int lowerBound = 1;
+        int upperBound = 100;
+
+        while (numbers.size() < 10) {
+            try {
+                lowerBound = readNumber(lowerBound, upperBound);
+
+                numbers.add(lowerBound);
+            } catch (NumbersException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        System.out.println(numbers.toString().replaceAll("[\\[\\]]", ""));
+    }
+
+    private static int readNumber(int lowerBound, int upperBound) {
+        String input = scanner.nextLine();
+        int num;
+
+        try {
+            num = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new NumbersException("Invalid Number!", e);
+        }
+
+        if (num <= lowerBound || num >= upperBound) {
+            String message = String.format("Your number is not in range %d - %d!",
+                    lowerBound, upperBound);
+            throw new NumbersException(message);
+        }
+
+        return num;
+    }
+}
---
+++ 4. Java-OOP-February-2023/13. exceptionsAndErrorHandling/exeptions/NumbersException.java
@@ -0,0 +1,12 @@
+package exceptionsAndErrorHandling.exeptions;
+
+public class NumbersException extends RuntimeException {
+    public NumbersException(String msg) {
+        super(msg);
+    }
+
+    public NumbersException(String msg, NumberFormatException cause) {
+        super(msg, cause);
+    }
+
+}

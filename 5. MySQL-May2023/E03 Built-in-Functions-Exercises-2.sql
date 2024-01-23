ef633cf1aed17a9214b63fc52deec8ef5724270b
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2024-01-23 22:05:17 +0200

    Update E03 Built-in-Functions-Exercises-2.sql

--- 5. MySQL-May2023/E03 Built-in-Functions-Exercises-2.sql
+++ 5. MySQL-May2023/E03 Built-in-Functions-Exercises-2.sql
@@ -1,14 +1,14 @@
-# 1. Find Names of All Employees by First Name
+-- Find Names of All Employees by First Name
 SELECT `first_name`, `last_name` from `employees`
 WHERE `first_name` LIKE 'Sa%'
 ORDER BY `employee_id`;
  
-# 2. Find Names of All Employees by Last Name
+-- Find Names of All Employees by Last Name
 SELECT `first_name`, `last_name` from `employees`
 WHERE `last_name` LIKE '%ei%'
 ORDER BY `employee_id`;
  
-# 3. Find First Names of All Employees
+-- Find First Names of All Employees
 SELECT `first_name` from `employees`
 WHERE `department_id` IN (3,10)
 AND 

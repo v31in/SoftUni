89e5208b7629cd09a9dd06474ece4c45f071d2e1
Velin Tsvetanov <V.tsvetanov@yahoo.com>
2024-01-25 16:05:13 +0200

    Update E03 Built-in-Functions-Exercises-2.sql

--- 5. MySQL-May2023/E03 Built-in-Functions-Exercises-2.sql
+++ 5. MySQL-May2023/E03 Built-in-Functions-Exercises-2.sql
@@ -1,56 +1,71 @@
--- Find Names of All Employees by First Name
-SELECT `first_name`, `last_name` from `employees`
-WHERE `first_name` LIKE 'Sa%'
-ORDER BY `employee_id`;
- 
--- Find Names of All Employees by Last Name
-SELECT `first_name`, `last_name` from `employees`
-WHERE `last_name` LIKE '%ei%'
-ORDER BY `employee_id`;
- 
--- Find First Names of All Employees
-SELECT `first_name` from `employees`
-WHERE `department_id` IN (3,10)
-AND 
-year(`hire_date`) BETWEEN 1995 and 2005
-ORDER BY `employee_id`;
- 
-# 4. Find All Employees Except Engineers
-SELECT `first_name`, `last_name` from `employees`
-WHERE `job_title` NOT LIKE '%engineer%'
-ORDER BY `employee_id`;
- 
-# 5. Find Towns with Name Length
-SELECT `name` from `towns`
+USE soft_uni;
+
+-- 1. Find Names of All Employees by First Name
+
+SELECT first_name, last_name FROM employees
+WHERE first_name LIKE "Sa%"
+ORDER BY employee_id;
+
+-- 2. Find Names of All Employees by Last Name
+
+SELECT first_name, last_name FROM employees
+WHERE last_name LIKE "%ei%"
+ORDER BY employee_id;
+
+-- 3. Find First Names of All Employees
+
+SELECT first_name FROM employees
+WHERE department_id IN (3, 10) 
+AND YEAR(hire_date) BETWEEN 1995 AND 2005
+ORDER BY employee_id;
+
+-- 4. Find All Employees Except Engineers
+
+SELECT first_name, last_name FROM employees
+WHERE job_title NOT LIKE '%engineer%'
+ORDER BY employee_id;
+
+-- 5. Find Towns with Name Length
+
+SELECT `name` FROM towns
 WHERE char_length(`name`) = 5 OR char_length(`name`) = 6
 ORDER BY `name`;
- 
-# 6. Find Towns Starting With
-SELECT * FROM `towns`
-WHERE `name` LIKE ('m%') OR `name` LIKE ('k%') OR `name` LIKE ('b%') OR `name` LIKE ('e%')
+
+-- 6. Find Towns Starting With letters M, K, B or E
+
+SELECT town_id, `name` FROM towns
+WHERE `name` LIKE "M%" OR `name` LIKE "K%" OR `name` LIKE "B%" OR `name` LIKE "E%"
 ORDER BY `name`;
- 
-# 7. Find Towns Not Starting With
-SELECT * FROM `towns`
-WHERE `name` NOT LIKE ('r%') AND `name` NOT LIKE ('b%') AND `name` NOT LIKE ('d%')
+
+-- 7. Find Towns Not Starting With letters R, B or D 
+
+SELECT town_id, `name` FROM towns
+WHERE `name` NOT LIKE "R%" AND `name` NOT LIKE "B%" AND `name` NOT LIKE "D%"
 ORDER BY `name`;
- 
-# 8. Create View Employees Hired After
-CREATE VIEW v_employees_hired_after_2000 as 
-SELECT `first_name`, `last_name` FROM `employees`
-WHERE  year(hire_date) > 2000;
+
+-- 8. Create View Employees Hired After 2000 Year
+
+CREATE VIEW v_employees_hired_after_2000 AS
+SELECT first_name, last_name
+FROM employees
+WHERE YEAR(hire_date) > 2000;
 SELECT * FROM v_employees_hired_after_2000;
- 
-# 9. Length of Last Name
-SELECT `first_name`, `last_name` FROM `employees`
-WHERE char_length(`last_name`) = 5;
- 
-# 10.   Countries Holding 'A' 3 or More Times
-SELECT `country_name`, `iso_code` FROM `countries`
-WHERE `country_name` LIKE '%a%a%a%'
-ORDER BY `iso_code`;
- 
-# 11. Mix of Peak and River Names
+
+-- 9. Length of Last Name
+
+SELECT first_name, last_name FROM employees
+WHERE char_length(last_name) = 5;
+
+USE geography;
+
+-- 10. Countries Holding 'A' 3 or More Times
+
+SELECT country_name, iso_code FROM countries
+WHERE country_name LIKE "%a%a%a%"
+ORDER BY iso_code;
+
+-- 11. Mix of Peak and River Names
+
 SELECT 
     `peak_name`,
     `river_name`,
@@ -61,41 +76,67 @@ FROM
 WHERE
     RIGHT(`peak_name`, 1) = LEFT(LOWER(`river_name`), 1)
 ORDER BY mix;
- 
-# 12. Games From 2011 and 2012 Year
-SELECT `name`, date_format(`start`, '%Y-%m-%d') AS `start` FROM `games`
-WHERE year(`start`) >= 2011 AND year(`start`) <= 2012  
-ORDER BY `start`, `name`
+
+use diablo;
+
+-- 12. Games from 2011 and 2012 Year
+
+SELECT 
+    `name`, DATE_FORMAT(`start`, '%Y-%m-%d') AS `start`
+FROM
+    `games`
+WHERE
+    YEAR(`start`) >= 2011
+        AND YEAR(`start`) <= 2012
+ORDER BY `start` , `name`
 LIMIT 50;
- 
-# 13. User Email Providers
-SELECT `user_name`, substring(`email`, locate('@', email) + 1) AS 'Email Provider' FROM `users` 
-ORDER BY `Email Provider`, `user_name`;
- 
-# 14. Get Users with IP Address Like Pattern
-SELECT `user_name`, `ip_address` FROM `users`
-WHERE `ip_address` LIKE '___.1%.%.___'
+
+-- 13. User Email Providers
+
+SELECT 
+    `user_name`,
+    SUBSTRING(`email`,
+        LOCATE('@', email) + 1) AS 'Email Provider'
+FROM
+    `users`
+ORDER BY `Email Provider` , `user_name`;
+
+-- 14. Get Users with IP Address Like Pattern
+
+SELECT 
+    `user_name`, `ip_address`
+FROM
+    `users`
+WHERE
+    `ip_address` LIKE '___.1%.%.___'
 ORDER BY `user_name`;
- 
-# 15. Show All Games with Duration
-SELECT `name` as 'game',
-CASE
-WHEN hour(`start`) < 12 THEN 'Morning'
-WHEN hour(`start`) < 18 THEN 'Afternoon'
-ELSE 'Evening'
-END 
-AS 'Part of the Day',
-CASE
-WHEN `duration` <= 3 THEN 'Extra Short'
-WHEN `duration` <= 6 THEN 'Short'
-WHEN `duration` <= 10 THEN 'Long'
-ELSE 'Extra Long'
-END 
-AS 'Duration'
-FROM `games`;
- 
-# 16. Orders Table
-SELECT `product_name`, `order_date`,
-adddate(`order_date`, interval 3 day) as pay_due, 
-adddate(`order_date`, interval 1 month) as deliver_due
-FROM `orders`;
+
+-- 15. Show All Games with Duration and Part of the Day
+
+SELECT 
+    `name` AS 'game',
+    CASE
+        WHEN HOUR(`start`) < 12 THEN 'Morning'
+        WHEN HOUR(`start`) < 18 THEN 'Afternoon'
+        ELSE 'Evening'
+    END AS 'Part of the Day',
+    CASE
+        WHEN `duration` <= 3 THEN 'Extra Short'
+        WHEN `duration` <= 6 THEN 'Short'
+        WHEN `duration` <= 10 THEN 'Long'
+        ELSE 'Extra Long'
+    END AS 'Duration'
+FROM
+    `games`;
+
+USE orders;
+
+-- 16. Orders Table
+
+SELECT 
+    `product_name`,
+    `order_date`,
+    ADDDATE(`order_date`, INTERVAL 3 DAY) AS pay_due,
+    ADDDATE(`order_date`, INTERVAL 1 MONTH) AS deliver_due
+FROM
+    `orders`;
